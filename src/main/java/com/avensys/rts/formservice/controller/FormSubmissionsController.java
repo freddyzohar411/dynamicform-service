415760a8e2075bca7d199103183187fe0ebcd4f1
hxg-avensys <H.Xiang@aven-sys.com>
2024-01-17 22:53:43 +0800

    Updated

--- src/main/java/com/avensys/rts/formservice/controller/FormSubmissionsController.java
+++ src/main/java/com/avensys/rts/formservice/controller/FormSubmissionsController.java
@@ -72,7 +72,7 @@ public class FormSubmissionsController {
     @GetMapping("/entity/{entityName}/names")
     public ResponseEntity<Object> getFormFieldNameList(@PathVariable String entityName) {
         log.info("Form Submission get: Controller");
-        return ResponseUtil.generateSuccessResponse(formSubmissionService.getFormFieldNameList(entityName), HttpStatus.OK, messageSource.getMessage(MessageConstants.MESSAGE_SUCCESS, null, LocaleContextHolder.getLocale()));
+        return ResponseUtil.generateSuccessResponse(formSubmissionService.getFormFieldNameList2(entityName), HttpStatus.OK, messageSource.getMessage(MessageConstants.MESSAGE_SUCCESS, null, LocaleContextHolder.getLocale()));
     }
 
     // Sorting and pagination
